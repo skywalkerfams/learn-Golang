c614b95a7b1cc84c9b4e16a3d99c65f5eb5e08dc
skywalkerfams <setyoaji98@gmail.com>
2021-10-19 13:39:15 +0800

    multiple comments

--- main.go
+++ main.go
@@ -4,4 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Ini tes ke Tiga save dari VSCode")
+	fmt.Println("I want to be success")
+
+	//Just a comment. Free to put anywhere and won't be show when run
+	//fmt func can't used outside the func main()
+
+	//func to print yes
+	fmt.Println("Yes")
+
+	//    /* words */ is a multiple comment.
+	/*
+		//func to print yes
+		fmt.Println("Yes")
+	*/
+
+	/* Variabel adalah suatu perintah untuk menampung sebuah value
+	contoh: var(spasi)nama_variabel(spasi)tipe_data(spasi)="x"
+	*/
+
+	var nama string = "Setyo Aji"
+	fmt.Println("My name is", nama)
+
 }
