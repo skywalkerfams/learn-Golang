4ff358df5b18d599834cdf6ef39f66fb108e235d
skywalkerfams <setyoaji98@gmail.com>
2021-10-19 14:19:56 +0800

    Tes pembanding data types

--- operators&ifelse.go
+++ operators&ifelse.go
@@ -3,6 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Tes operators")
+	fmt.Println("Learn operators")
 
+	/* if statement
+	==
+	!=
+	>=
+	<=
+	>
+	<
+	*/
+
+	nama := "Setyo"
+
+	if nama == "Setyo" {
+		fmt.Println("Yes his name is true")
+	} else {
+		fmt.Println("Not same with the variables")
+	}
+	/*Karena disini saya punya variabel yang dimana nama := "Setyo"
+	== if nama Setyo maka hasilnya pasti benar. Jika salah, maka
+	akan diproses di else. Sebagai latihan, bisa ganti value dari if nama == "Aji"
+	lalu save kemudian run lagi codenya. Maka akan diproses di else
+	*/
+	/*Untuk menggunakan data types tipe string, valuenya harus sama.
+	begitu juga integer tipe datanya harus integer, boleans dgn boleans,
+	float dan float. Tipe data harus sama untuk membandingkan sesuatu.
+
+	/* coba dihapus multiple comment yang ini
+		if 1 == "1" {
+			fmt.Println("Apakah sama?")
+		}
+		maka ketika di run hasilnya:
+		invalid operation: 1 == "1" (mismatched types untyped int and untyped string)
+	*/
 }
