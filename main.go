d62a1d6bcfdb784cef5a076ff2fde2a71fbb8d3c
skywalkerfams <setyoaji98@gmail.com>
2021-10-19 13:58:43 +0800

    Var and Const

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Ini tes ke Tiga save dari VSCode")
@@ -24,7 +26,28 @@ func main() {
 
 	var nama string = "Setyo Aji"
 	var umur int = 22
+	var bulin bool = true
 
 	fmt.Println("My name is", nama, "and I'm", umur, "years old")
+	fmt.Println("The statement above is", bulin)
+
+	// Constanta atau line code : const harus memiliki nama_variabel yang berbeda
+	const tempat_tinggal string = "Nuri Street No 15"
+	fmt.Println("I lived in", tempat_tinggal)
+
+	umur = 17
+	fmt.Println("My name is", nama, "and I'm", umur, "years old")
+	/*kenapa yang di print umur 17 dan bukan 22?
+	karena sudah beda variabel (re-assign).
+	Kasih beda contoh lagi*/
+
+	tempat_tinggal = "Nuri Street No 51"
+	fmt.Println("I lived in", tempat_tinggal)
+	/*muncul error. Cannot assign to tempat_tinggal (declared const)
+	Bedanya const dan variabel, kalau const kalau sudah dideklarasi dan sdh diassign
+	valuenya/isinya tidak boleh lagi dikasih value/assign yang berbeda. Kalau variabel 
+	bisa diganti/reassign valuen= atau nilainya. Itu aja bedanya. Kenapa umur bisa terganti?
+	karena umur itu variabel awal deklarasinya. 
+
 
 }
