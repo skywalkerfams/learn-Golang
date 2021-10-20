b8e6cbdd307c940836f92ace344fcef6b61fc575
skywalkerfams <setyoaji98@gmail.com>
2021-10-20 22:48:43 +0800

    Commit rename file

--- 4.array.go
+++ 4.array.go
@@ -16,12 +16,25 @@ func main() {
 	ingat, array memiliki index, maka buat seperti dibawah ini */
 	var a [7]int
 
-	a[0] = 1
-	a[1] = 2
-	a[3] = 3
-	a[4] = 4
-	a[5] = 5
-	a[6] = 6
-	fmt.Println(a) //Jika di run hasilnya akan seperti ini "[1 2 0 3 4 5 6]""
+	a[0] = 10
+	a[1] = 20
+	a[2] = 30
+	a[3] = 40
+	a[4] = 50
+	a[5] = 60
+	a[6] = 70
+	fmt.Println(a) //Jika di run hasilnya akan seperti ini "[10 20 30 40 50 60 70]"
 
+	var counterpain [5]string //nama variabel bisa bebas, kebetulan lg pakai counterpain
+
+	counterpain[0] = "Methyl salicylate"
+	counterpain[1] = "Eugenol"
+	counterpain[2] = "Menthol"
+	counterpain[3] = "Vanishing cream base to"
+	counterpain[4] = "Analgesic cream"
+	fmt.Println(counterpain)
+
+	fmt.Println(counterpain[1])
+	fmt.Println(counterpain[2])
+	fmt.Println(counterpain[4])
 }
